b427d7d6cc5e1cda63bf63d3a33bc9c213028cb6
myselfvenky <venkateshdonthula2002@gmail.com>
2023-07-10 02:33:25 +0530

    Flutter Engine initiated

---
+++ src/main/java/com/varcsoft/flutterengine/FlutterEngineApplication.java
@@ -0,0 +1,13 @@
+package com.varcsoft.flutterengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterEngineApplication.class, args);
+	}
+
+}
